71e498eb9b3d1b9794ef1dc8a4471eeb6d3b9ed2
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-07-27 14:08:57 +0700

    T5 - 1 - app - remove unnecessary routes(they were created for testing at an early stage)

--- app/App.jsx
+++ app/App.jsx
@@ -1,7 +1,5 @@
 import {Route, Routes} from 'react-router-dom';
 import React from 'react';
-import Header from "./components/header.jsx";
-import Article from "./components/article.jsx";
 import PageDoesNotExist from "./Page/pageDoesNotExist.jsx";
 import UserTable from "./components/userTable.jsx";
 import EventTable from "./components/eventTable.jsx";
@@ -10,11 +8,10 @@ import RequestTable from "./components/requestTable.jsx";
 function App() {
     return (
         <Routes>
-            <Route path="/admin" element={<Header/>}/>
+            <Route path="/admin" element={<UserTable/>}/>
             <Route path="/admin/user" element={<UserTable/>}/>
             <Route path="/admin/event" element={<EventTable/>}/>
             <Route path="/admin/request" element={<RequestTable/>}/>
-            <Route path="/admin/test" element={<Article/>}/>
             <Route path="*" element={<PageDoesNotExist/>}/>
         </Routes>
     )
