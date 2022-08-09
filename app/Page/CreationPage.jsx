d22fe6f1dde16343c977dac698a9a17de6317d44
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-08-09 20:04:04 +0700

    T6 - 5 - page - move the common functionality for the same page types into separate functions

---
+++ app/Page/CreationPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "../components/navbar.jsx";
+import EventCreate from "../components/eventCreate.jsx";
+import RequestCreate from "../components/requestCreate.jsx";
+import UserCreate from "../components/userCreate.jsx";
+
+function CreationPage(props) {
+
+    var entityOfCreation =
+        {
+            'user': UserCreate,
+            'event': EventCreate,
+            'request': RequestCreate,
+        }
+
+    var navbarActiveTabs =
+        {
+            'user': '',
+            'event': '',
+            'request': '',
+        };
+
+    var Component = entityOfCreation[props.entity];
+
+    return (
+        <>
+            <Navbar navbarActiveTabs={navbarActiveTabs}/>
+            <Component/>
+        </>
+    )
+}
+
+export default CreationPage;
+
---
+++ app/Page/PageWithDataTable.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../components/navbar.jsx";
+import DataTable from "../components/DataTable.jsx";
+
+function PageWithDataTable(props) {
+
+    var NavbarActiveTabs =
+        {
+            'user': '',
+            'event': '',
+            'request': '',
+        };
+
+    NavbarActiveTabs[props.entity] = 'active';
+
+    return (
+        <>
+            <Navbar navbarActiveTabs={NavbarActiveTabs}/>
+            <DataTable entity={props.entity}/>
+        </>
+    )
+}
+
+export default PageWithDataTable;
