5d3bdaa07e9e411863560023e5b75c3514b3ade6
Svyatoslav <svyatoslav_muravlev@dotwrk.com>
2022-08-26 19:08:13 +0700

    T8 - 5 - app, page - add visual effect related to show of loading page. and protect from opening page for not administrator

--- app/app.tsx
+++ app/app.tsx
@@ -1,23 +1,60 @@
 import {Route, Routes} from 'react-router-dom';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PageDoesNotExist from "./page/pageDoesNotExist";
 import CreationPage from "./page/creationPage";
 import PageWithDataTable from "./page/pageWithDataTable";
+import FetchRequest from "./components/jsFunctions/fetchRequest";
+import LoadingPage from "./page/loadingPage";
+
 
 function App() {
+    const [adminStatus, setAdminStatus] = useState('loading');
+    useEffect(() => {
+        FetchRequest(
+            JSON.stringify(
+                []
+            ),
+            "POST",
+            '/userApiCheckAdminRights'
+        )
+            .then(response => response.json())
+            .then((data) => {
+                    setAdminStatus(data.status);
+                });
+    }, []);
+
+    if (adminStatus === 'loading') {
+     return (
+         <LoadingPage/>
+     );
+    } else if (adminStatus === 'success') {
         return (
             <Routes>
-                <Route path="/admin/event/create" element={<CreationPage entity={'event'} title={"Создать ивент"}/>}/>
-                <Route path={"/admin/event/:id"} element={<CreationPage entity={'event'} title={"Редактировать ивент"}/>}/>
-                <Route path="/admin/request/create" element={<CreationPage entity={'request'} title={"Создать запрос"}/>}/>
-                <Route path="/admin/user/create" element={<CreationPage entity={'user'} title={"Создать пользователя"}/>}/>
+                <Route path="/admin/event/create"
+                       element={<CreationPage entity={'event'} title={"Создать ивент"}/>}/>
+                <Route path={"/admin/event/:id"}
+                       element={<CreationPage entity={'event'} title={"Редактировать ивент"}/>}/>
+                <Route path="/admin/request/create"
+                       element={<CreationPage entity={'request'} title={"Создать запрос"}/>}/>
+                <Route path="/admin/user/create"
+                       element={<CreationPage entity={'user'} title={"Создать пользователя"}/>}/>
                 <Route path="/admin" element={<PageWithDataTable entity={'user'} title={"Список пользователей"}/>}/>
-                <Route path="/admin/user" element={<PageWithDataTable entity={'user'} title={"Список пользователей"}/>}/>
-                <Route path="/admin/event" element={<PageWithDataTable entity={'event'} title={"Список ивентов"}/>}/>
-                <Route path="/admin/request" element={<PageWithDataTable entity={'request'} title={"Список запросов"}/>}/>
+                <Route path="/admin/user"
+                       element={<PageWithDataTable entity={'user'} title={"Список пользователей"}/>}/>
+                <Route path="/admin/event"
+                       element={<PageWithDataTable entity={'event'} title={"Список ивентов"}/>}/>
+                <Route path="/admin/request"
+                       element={<PageWithDataTable entity={'request'} title={"Список запросов"}/>}/>
+                <Route path="*" element={<PageDoesNotExist/>}/>
+            </Routes>
+        );
+    } else {
+        return (
+            <Routes>
                 <Route path="*" element={<PageDoesNotExist/>}/>
             </Routes>
         )
+    }
 }
 
 export default App;
---
+++ app/page/loadingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoadingPage() {
+    return (
+
+        <div className="d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
+            <img className="image" width="300px"
+                     src="/admin/public/images/loading.gif"/>
+        </div>
+    )
+}
+
+export default LoadingPage;
